12a50bf16dcf5725cc05d5c2f2b2c30e67cdb284
Hiep Pham <phamminhhiep280690@gmail.com>
2019-06-05 18:21:14 +0200

    different branch depth for a model

--- deep_learning/models.py
+++ deep_learning/models.py
@@ -1,3 +1,4 @@
+# inspired from https://www.pyimagesearch.com/2018/06/04/keras-multiple-outputs-and-multiple-losses/
 # import the necessary packages
 from keras.models import Model
 from keras.layers.normalization import BatchNormalization
@@ -17,6 +18,7 @@ lb.reload(generators)
 class MediumModel:
     
     def build_a_branch(inputs, numclasses =26, kernel_size = (20,11), finalAct="softmax", name = None):
+        
         # CONV => RELU => POOL
         x = Conv2D(filters=32,kernel_size= kernel_size,padding='same')(inputs)
         x = Activation("relu")(x)
@@ -47,6 +49,55 @@ class MediumModel:
         x = Activation(finalAct, name=name)(x)
 
         return x
+    # a deeper/longer branch to handle letters in the middle
+    def build_a_deep_branch(inputs, numclasses =26, kernel_size = (20,11), finalAct="softmax", name = None):
+        
+        # FIRST CONVOL BLOCK: conv -> relu -> pool
+        x = Conv2D(filters=32,kernel_size= kernel_size,padding='same')(inputs)
+        x = Activation("relu")(x)
+        x = BatchNormalization()(x)
+        x = MaxPooling2D(pool_size=(3, 3))(x)
+        x = Dropout(0.25)(x)
+
+        # SECOND CONVOL BLOCK: conv -> relu -> pool
+        x = Conv2D(32, kernel_size, padding="same")(x)
+        x = Activation("relu")(x)
+        x = BatchNormalization()(x)
+        x = MaxPooling2D(pool_size=(2, 2))(x)
+        x = Dropout(0.25)(x)
+
+        # THIRD CONVOL BLOCK: conv -> relu -> pool
+        x = Conv2D(32, kernel_size, padding="same")(x)
+        x = Activation("relu")(x)
+        x = BatchNormalization()(x)
+        x = MaxPooling2D(pool_size=(2, 2))(x)
+        x = Dropout(0.25)(x)
+
+        # FOURTH CONVOL BLOCK: conv -> relu -> pool
+        x = Conv2D(32, kernel_size, padding="same")(x)
+        x = Activation("relu")(x)
+        x = BatchNormalization()(x)
+        x = MaxPooling2D(pool_size=(2, 2))(x)
+        x = Dropout(0.25)(x)
+        
+        # FIRST DENSE LAYER
+        x = Flatten()(x)
+        x = Dense(256)(x)
+        x = Activation("relu")(x)
+        x = BatchNormalization()(x)
+        x = Dropout(0.5)(x)
+        
+        # SECOND DESNE LAYER
+        x = Dense(128)(x)
+        x = Activation("relu")(x)
+        x = BatchNormalization()(x)
+        x = Dropout(0.5)(x)
+        
+        # OUTPUT LAYER
+        x = Dense(numclasses)(x)
+        x = Activation(finalAct, name=name)(x)
+
+        return x
 
     @staticmethod
     def build(width = 200, height = 20, numclasses =26, kernel_size = (20,11), finalAct="softmax"):
@@ -69,7 +120,29 @@ class MediumModel:
         # return the constructed network architecture
         
         return model
-      
+     
+     @staticmethod
+     def build_diff_length(width = 200, height = 20, numclasses =26, kernel_size = (20,11), finalAct="softmax"):
+        
+        inputShape = (height, width, 1)
+        inputs = Input(shape=inputShape)
+        
+        first  = MediumModel.build_a_branch(inputs, numclasses = numclasses, kernel_size= kernel_size, finalAct=finalAct, name = 'first')
+        second = MediumModel.build_a_branch(inputs, numclasses = numclasses, kernel_size= kernel_size, finalAct=finalAct, name = 'second')
+        third  = MediumModel.build_a_deep_branch(inputs, numclasses = numclasses, kernel_size= kernel_size, finalAct=finalAct, name = 'third')
+        fourth = MediumModel.build_a_deep_branch(inputs, numclasses = numclasses, kernel_size= kernel_size, finalAct=finalAct, name = 'fourth')
+        fifth  = MediumModel.build_a_deep_branch(inputs, numclasses = numclasses, kernel_size= kernel_size, finalAct=finalAct, name = 'fifth')
+        sixth  = MediumModel.build_a_deep_branch(inputs, numclasses = numclasses, kernel_size= kernel_size, finalAct=finalAct, name = 'sixth')
+        seventh= MediumModel.build_a_deep_branch(inputs, numclasses = numclasses, kernel_size= kernel_size, finalAct=finalAct, name = 'seventh')
+        eighth = MediumModel.build_a_deep_branch(inputs, numclasses = numclasses, kernel_size= kernel_size, finalAct=finalAct, name = 'eighth')
+        nineth = MediumModel.build_a_branch(inputs, numclasses = numclasses, kernel_size= kernel_size, finalAct=finalAct, name = 'nineth')
+        tenth  = MediumModel.build_a_branch(inputs, numclasses = numclasses, kernel_size= kernel_size, finalAct=finalAct, name = 'tenth')
+        outputs=[first, second, third, fourth, fifth, sixth, seventh, eighth, nineth, tenth]
+        model = Model(inputs=inputs,outputs= outputs, name="ten outputs")
+        # return the constructed network architecture
+        
+        return model
+ 
 class DeepModel:
     
     def build_a_branch(inputs, numclasses =26, kernel_size = (20,11), finalAct="softmax", name = None):
